fa12cce9ed10af2b976f35df38f5f51ecc725c55
ManchesterUK02 <134781655+ManchesterUK02@users.noreply.github.com>
2024-04-23 23:51:30 +0530

    Update inputmergeomp.cpp

--- inputmergeomp.cpp
+++ inputmergeomp.cpp
@@ -1,82 +1,117 @@
 #include <iostream>
 #include <vector>
-#include <omp.h>
+#include <cstdlib>  // For rand()
+#include <ctime>    // For clock()
+#include <omp.h>    // OpenMP
 
-using namespace std;
+// Sequential Merge Sort
+void mergeSortSequential(int* arr, int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+        mergeSortSequential(arr, left, mid);
+        mergeSortSequential(arr, mid + 1, right);
 
-void merge(vector<int>& arr, int l, int m, int r) {
-    int i, j, k;
-    int n1 = m - l + 1;
-    int n2 = r - m;
-    vector<int> L(n1), R(n2);
-    for (i = 0; i < n1; i++) {
-        L[i] = arr[l + i];
-    }
-    for (j = 0; j < n2; j++) {
-        R[j] = arr[m + 1 + j];
-    }
-    i = 0;
-    j = 0;
-    k = l;
-    while (i < n1 && j < n2) {
-        if (L[i] <= R[j]) {
-            arr[k++] = L[i++];
-        } else {
-            arr[k++] = R[j++];
+        // Merge the sorted halves
+        int temp[right - left + 1];
+        int i = left, j = mid + 1, k = 0;
+
+        while (i <= mid && j <= right) {
+            if (arr[i] <= arr[j]) {
+                temp[k++] = arr[i++];
+            } else {
+                temp[k++] = arr[j++];
+            }
+        }
+
+        while (i <= mid) {
+            temp[k++] = arr[i++];
+        }
+
+        while (j <= right) {
+            temp[k++] = arr[j++];
+        }
+
+        for (int p = 0; p < k; p++) {
+            arr[left + p] = temp[p];
         }
     }
-    while (i < n1) {
-        arr[k++] = L[i++];
-    }
-    while (j < n2) {
-        arr[k++] = R[j++];
-    }
 }
 
-void merge_sort(vector<int>& arr, int l, int r) {
-    if (l < r) {
-        int m = l + (r - l) / 2;
-#pragma omp task
-        merge_sort(arr, l, m);
-#pragma omp task
-        merge_sort(arr, m + 1, r);
-#pragma omp taskwait
-        merge(arr, l, m, r);
-    }
-}
+// Parallel Merge Sort using OpenMP
+void mergeSortParallel(int* arr, int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
 
-void parallel_merge_sort(vector<int>& arr) {
-#pragma omp parallel
-    {
-#pragma omp single
-        merge_sort(arr, 0, arr.size() - 1);
+        #pragma omp parallel sections
+        {
+            #pragma omp section
+            mergeSortParallel(arr, left, mid);
+            #pragma omp section
+            mergeSortParallel(arr, mid + 1, right);
+        }
+
+        // Merge the sorted halves
+        int temp[right - left + 1];
+        int i = left, j = mid + 1, k = 0;
+
+        while (i <= mid && j <= right) {
+            if (arr[i] <= arr[j]) {
+                temp[k++] = arr[i++];
+            } else {
+                temp[k++] = arr[j++];
+            }
+        }
+
+        while (i <= mid) {
+            temp[k++] = arr[i++];
+        }
+
+        while (j <= right) {
+            temp[k++] = arr[j++];
+        }
+
+        for (int p = 0; p < k; p++) {
+            arr[left + p] = temp[p];
+        }
     }
 }
 
 int main() {
-    int n;
-    cout << "Enter the number of elements: ";
-    cin >> n;
-    vector<int> arr(n);
-    cout << "Enter " << n << " elements: ";
-    for (int i = 0; i < n; ++i) {
-        cin >> arr[i];
+    /*
+    const int size = 1000;
+    int arr[size];
+    for (int i = 0; i < size; i++) {
+        arr[i] = rand() % 1000;  // Random numbers between 0 and 999
     }
+    */
+    
+    int size;
+    std::cout << "Enter the number of elements: ";
+    std::cin >> size;
 
-    double start, end;
+    int arr[size];
+    std::cout << "Enter " << size << " integers:" << std::endl;
+    for (int i = 0; i < size; i++) {
+        std::cin >> arr[i];
+    }
+    
+    clock_t startSeqMerge = clock();
+    mergeSortSequential(arr, 0, size - 1);
+    clock_t endSeqMerge = clock();
+    double timeSeqMerge = double(endSeqMerge - startSeqMerge) / CLOCKS_PER_SEC;
 
-    // Measure performance of sequential merge sort
-    start = omp_get_wtime();
-    merge_sort(arr, 0, arr.size() - 1);
-    end = omp_get_wtime();
-    cout << "Sequential merge sort time: " << end - start << " seconds" << endl;
+    clock_t startParMerge = clock();
+    mergeSortParallel(arr, 0, size - 1);
+    clock_t endParMerge = clock();
+    double timeParMerge = double(endParMerge - startParMerge) / CLOCKS_PER_SEC;
 
-    // Measure performance of parallel merge sort
-    start = omp_get_wtime();
-    parallel_merge_sort(arr);
-    end = omp_get_wtime();
-    cout << "Parallel merge sort time: " << end - start << " seconds" << endl;
+    // Print execution times
+    std::cout << "Sequential Merge Sort Time: " << timeSeqMerge << " seconds" << std::endl;
+    std::cout << "Parallel Merge Sort Time: " << timeParMerge << " seconds" << std::endl;
+
+    for(int i = 0; i<size;i++){
+        std :: cout << arr[i] << " ";
+    }
 
     return 0;
 }
-
